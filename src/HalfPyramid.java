cd1235f0bad237a86afc57ba03d34c4e97c4532b
Visal <bestlost3@gmail.com>
2023-04-25 11:31:19 +0530

    Loop

---
+++ src/HalfPyramid.java
@@ -0,0 +1,10 @@
+public class HalfPyramid {
+    public static void main(String[] args) {
+        for (int i=1 ; i<7 ; i++){
+            for(int n=1 ; n<i ; n++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
